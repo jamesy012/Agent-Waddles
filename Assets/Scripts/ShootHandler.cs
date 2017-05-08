8a51f1decfed8a9fd4a954d6a5df0efca7a65180
James Owen <jamesowen012@gmail.com>
2017-05-08 12:01:50 +1000

    Basic Shooting using forward from flipper

---
+++ Assets/Scripts/ShootHandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootHandler : MonoBehaviour {
+
+	public Transform m_ShootPoint;
+	public Transform m_ShootArm;
+	public Vector3 m_ShootCameraOffsetRotation = new Vector3(90, -180, 0);
+
+	public float m_ArmRotationSpeed = 5;
+	private Quaternion m_StartingRot;
+
+	// Use this for initialization
+	void Start() {
+		m_StartingRot = m_ShootArm.rotation;
+	}
+
+	// Update is called once per frame
+	void Update() {
+		bool isAiming = false;
+		Quaternion desiredRotation;
+		if (Input.GetButton("Fire2")) {
+			isAiming = true;
+		}
+		if (isAiming) {
+			desiredRotation = Camera.main.transform.rotation * Quaternion.Euler(m_ShootCameraOffsetRotation);
+		} else {
+			desiredRotation = m_StartingRot;
+		}
+		m_ShootArm.rotation = Quaternion.Slerp(m_ShootArm.rotation, desiredRotation, Time.deltaTime * m_ArmRotationSpeed);
+
+
+		if (isAiming) {
+			if (Input.GetButtonDown("Fire1")) {
+				Debug.DrawRay(m_ShootPoint.position, m_ShootPoint.forward * 1000, Color.red, 5);
+			}
+		}
+	}
+}
