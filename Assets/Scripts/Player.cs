047b07ead5291182bfe574946eaa56f0b8277ce2
Vincent Chin <vince.chin.aie@gmail.com>
2017-05-09 13:30:57 +1000

    made ground thicker

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,7 +13,7 @@ public class Player : MonoBehaviour
     public bool m_jumping;
     public bool m_prone;
 
-   
+
     // Use this for initialization
     void Start()
     {
@@ -25,16 +25,16 @@ public class Player : MonoBehaviour
     void Update()
     {
         //m_jumping = false;
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             m_jumping = true;
         }
-        if(Input.GetKeyDown(KeyCode.C))
+        if (Input.GetKeyDown(KeyCode.C))
         {
             m_prone ^= true;
         }
 
-   
+
 
         movement = this.transform.right * Input.GetAxis("Horizontal") + this.transform.forward * Input.GetAxis("Vertical");
     }
@@ -43,12 +43,15 @@ public class Player : MonoBehaviour
     {
         movement.y = 0;
 
-        if(movement.sqrMagnitude != 0.0f)
+        if (movement.sqrMagnitude != 0.0f)
         {
             m_walking = true;
         }
         else
         {
+            //less slippery
+            //m_rigidBody.velocity -= new Vector3(m_rigidBody.velocity.x * 0.9f, 0, m_rigidBody.velocity.z * 0.9f);
+
             m_walking = false;
         }
 
@@ -60,15 +63,15 @@ public class Player : MonoBehaviour
         if (m_prone)
         {
             Vector3 proneMovement = this.transform.right * Input.GetAxis("Horizontal") + this.transform.up * Input.GetAxis("Vertical");
-            m_rigidBody.transform.localRotation = Quaternion.RotateTowards(m_rigidBody.transform.localRotation, m_proneRotation , 10);
+            m_rigidBody.transform.localRotation = Quaternion.RotateTowards(m_rigidBody.transform.localRotation, m_proneRotation, 10);
             m_rigidBody.transform.up = camDir;
-            
+
             m_rigidBody.AddForce(proneMovement.normalized * m_speed);
         }
         else
         {
- 
-            m_rigidBody.transform.rotation = Quaternion.RotateTowards(m_rigidBody.transform.localRotation, Quaternion.LookRotation(camDir), 10) ;
+
+            m_rigidBody.transform.rotation = Quaternion.RotateTowards(m_rigidBody.transform.localRotation, Quaternion.LookRotation(camDir), 10);
             m_rigidBody.AddForce(movement.normalized * m_speed);
         }
     }
