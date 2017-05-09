05d21e6588fe0b9605edcc7ae0edcaeb59748463
Vincent Chin <vince.chin.aie@gmail.com>
2017-05-09 10:04:28 +1000

    made waddle forward animation

---
+++ Assets/Scripts/AnimationHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationHandler : MonoBehaviour
+{
+    private Animator m_animator;
+    // Use this for initialization
+    void Start()
+    {
+        m_animator = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            m_animator.SetBool("WalkingForward", true);
+        }
+        else
+        {
+            m_animator.SetBool("WalkingForward", false);
+        }
+    }
+}
