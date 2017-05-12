b3c7237c04e7464a789e811254e41a4632c1b97f
AIE\s171728 <vince.chin.aie@gmail.com>
2017-05-12 15:06:08 +1000

    head rotates with slide toggle

---
+++ Assets/FaceCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceCamera : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 camDir = -Camera.main.transform.right;
+
+        this.transform.rotation = Quaternion.RotateTowards(this.transform.rotation, Quaternion.LookRotation(camDir), 10);
+    }
+}
